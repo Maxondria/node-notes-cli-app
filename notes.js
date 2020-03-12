0a89b6e8c71ecfb63a3baa889ec7d46e02b61a5e
Maxondria <maxtayebwa@gmail.com>
2020-03-12 23:30:03 +0300

    Refactor to arrow functions

--- notes.js
+++ notes.js
@@ -5,7 +5,7 @@ const getNotes = function() {
   return "Your notes...";
 };
 
-const addNote = function(title, body) {
+const addNote = (title, body) => {
   const notes = loadNotes();
   const duplicateNotes = notes.filter(note => note.title === title);
   if (duplicateNotes.length === 0) {
@@ -17,18 +17,18 @@ const addNote = function(title, body) {
   }
 };
 
-const removeNote = function(title) {
+const removeNote = title => {
   const notes = loadNotes();
   saveNotes(notes.filter(note => note.title !== title));
   console.log(chalk.green.inverse("Note Removed successfully!"));
 };
 
-const saveNotes = function(notes) {
+const saveNotes = notes => {
   const dataJSON = JSON.stringify(notes);
   fs.writeFileSync("notes.json", dataJSON);
 };
 
-const loadNotes = function() {
+const loadNotes = () => {
   try {
     const dataBuffer = fs.readFileSync("notes.json");
     const dataJSON = dataBuffer.toString();
